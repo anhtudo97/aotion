5fcfb3c996133aa35582f13d4c695502c74873d8
anhtudo97 <anhtudo97@gmail.com>
2023-10-14 17:21:49 +0700

    feat: marketing updated

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -34,14 +34,14 @@ export const Navbar = () => {
               </Button>
             </SignInButton>
             <SignInButton mode="modal">
-              <Button size="sm">Get Jotion free</Button>
+              <Button size="sm">Get Aotion free</Button>
             </SignInButton>
           </>
         )}
         {isAuthenticated && !isLoading && (
           <>
             <Button variant="ghost" size="sm" asChild>
-              <Link href="/documents">Enter Jotion</Link>
+              <Link href="/documents">Enter Aotion</Link>
             </Button>
             <UserButton afterSignOutUrl="/" />
           </>
