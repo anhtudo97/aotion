8d27eb7b0536739e68528cea236690a337aeda54
anhtudo97 <anhtudo97@gmail.com>
2023-10-17 02:21:08 +0700

    feat(layout): update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,18 @@
-import { ThemeProvider } from '@/components/providers/theme.provider';
-import './globals.css';
-import type { Metadata } from 'next';
+import { Toaster } from 'sonner';
 import { Inter } from 'next/font/google';
+import type { Metadata } from 'next';
+
+import { ThemeProvider } from '@/components/providers/theme.provider';
 import { ConvexClientProvider } from '@/components/providers/convex.provider';
+import { ModalProvider } from '@/components/providers/modal.provider';
+import { EdgeStoreProvider } from '@/lib/edgestore';
+
+import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Aotion',
+  title: 'Jotion',
   description: 'The connected workspace where better, faster work happens.',
   icons: {
     icon: [
@@ -34,15 +39,19 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <ConvexClientProvider>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-            storageKey="jotion-theme-2"
-          >
-            {children}
-          </ThemeProvider>
+          <EdgeStoreProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+              storageKey="jotion-theme-2"
+            >
+              <Toaster position="bottom-center" />
+              <ModalProvider />
+              {children}
+            </ThemeProvider>
+          </EdgeStoreProvider>
         </ConvexClientProvider>
       </body>
     </html>
